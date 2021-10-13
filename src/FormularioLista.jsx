506c901d743f07b4c7fa7018b1b3f8c84cd66a37
Alex Herrera <axherrerax@gmail.com>
2021-10-13 01:50:32 -0300

    Lista tareas terminado

--- src/FormularioLista.jsx
+++ src/FormularioLista.jsx
@@ -1,110 +1,125 @@
 import React from 'react'
 import { useState } from 'react'
 
+
 const FormularioLista = () => {
 
-    const [tarea, setTarea] = useState("");
-    const [lista, setlista] = useState([]);
-    const [contador, setContador] = useState(0);
-    const [editarLista, setEditarLista] = useState(false)
+
+const [tarea, setTarea] = useState("");
+const [lista, setLista] = useState([]);
+const [contador, setContador] =useState(0);
+const [estadoEditar, setestadoEditar] = useState(false);
+const [idUnico, setIdUnico] = useState("");
+const [error, setError] = useState(null);
+
+
+
+
+const enviarDatos = (e) =>{
+    
+e.preventDefault();
+if (!tarea.trim()){
+        console.log("Ingrese dato");
+        setError("Ingrese tarea")
+        return;
+    }
+
+setContador(contador+1);
+setLista([...lista,{id:contador, nombreTarea:tarea}])
+setTarea("");
+setError(null)
+}
+
+
+const eliminar = (id) =>{
+const filtrado = lista.filter(item => item.id !==id);
+setLista(filtrado);
+}
+
+const editar =(item) =>{
+    setestadoEditar(true)
+    setTarea(item.nombreTarea)
+    console.log(item.nombreTarea);
+    setIdUnico(item.id)
+    
+    
+}
+
+const cambio =(e) =>{
+    e.preventDefault();
+if (!tarea.trim()){
+        console.log("Ingrese dato");
+        setError("Ingrese tarea")
+        return;
+    }
 
     
+   const recorrido = lista.map(item =>(
+       item.id === idUnico ? {id:idUnico, nombreTarea:tarea} : item
+   ))    
+setLista(recorrido);
+setestadoEditar(false);
+setTarea("");
+setError(null)
 
+}
+    
 
-    const enviarDatos = (e) => {
-        setContador(contador + 1)
-        e.preventDefault();
-
-        if (!tarea.trim()) {
-            console.log("Falta");
-            return;
-        }
-
-
-
-        setlista([...lista, { id: contador, nombreTarea: tarea }])
-
-        //e.target.reset();
-        setTarea("");
-
-      
-        }
-
-  const eliminar = (id) =>{
-      console.log(id);
-
-      const filtrado = lista.filter(item => item.id !==id )
-      console.log(filtrado);
-      setlista(filtrado)
-    }
-
-    const editar = (item) =>{
-
-        setEditarLista(true);
-        console.log(item);
-        setTarea(item.nombreTarea)
-        
-      
-    }
 
     return (
         <>
 
             <div className="container">
-                <h1 className="text-center mb-4">Crud Simple</h1>
                 <div className="row">
-                    <div className="col-7 border">
-                        <h4 className="text-center mb-4">Lista de tareas</h4>
-
+                    <div className="col-7 ">
+                        <h2 className="text-center mb-4">Lista de tareas</h2>
                         <ul className="list-group">
 
+                            {/* COMIENZA LISTA DE TAREAS */}
 
-                            {
-                                lista.length === 0
-                                    ? <>  <span className="lead">No hay tareas</span> </>
-                                    : (
-                                        lista.map((item) => (
+                             {
+                                 lista.length===0
+                                 ?
+                                 <span className="lead border p-1">No hay tareas</span>
+                                :
 
-                                            <li className="list-group-item " key={item.id}>
-                                                <span className="lead">{item.nombreTarea}</span>
-                                                <button type="button" className="btn btn-danger float-end mx-2" onClick={()=>eliminar(item.id)}>Eliminar</button>
-                                                <button type="button" className="btn btn-warning float-end" onClick={() => editar(item)}>Editar</button>
+                                 lista.map((item) => (
+                                    
+                             <li className="list-group-item" key={item.id}>
+                             <span className="lead">{item.nombreTarea}</span>
 
-                                            </li>
+                             <button className="btn btn-warning float-end mx-3" onClick={()=>editar(item)}>Editar</button>
 
-                                        ))
-                                    )
+                             <button className="btn btn-danger float-end" onClick={()=>eliminar(item.id)}>Eliminar</button>
+                             </li>
 
+                                ))
 
-                            }
+                             }                       
 
+                            
 
-
+                             {/* FINALIZA LISTA DE TAREAS */}
                         </ul>
-
-
                     </div>
 
-                    <div className="col-5 border">
+                    <div className="col-5 ">
 
-
-                        <h4 className="text-center">{editarLista ? "Editar tarea" : "Agregar tarea"}</h4>
-
-
+                       {/*  COMIENZA INGRESO DE TAREA */}
+                        <h2 className="text-center mb-4">Agregar tarea</h2>
+                        {error && <span className="text-danger">{error}</span> }
                         <form onSubmit={enviarDatos}>
-                            <input
-                                type="text"
-                                className="form-control mt-3 mb-2"
-                                placeholder="Ingrese Tarea"
-                                onChange={(e) => setTarea(e.target.value)}
-                               value={tarea}
-                            />
-                           {
-                               editarLista ? ( <button type="submit" className="btn btn-warning ">Editar</button>) :
-                               ( <button type="submit" className="btn btn-primary ">Agregar</button>)
-                           }
+                            <input  onChange={(e) =>  setTarea(e.target.value)} value={tarea}  type="text" className="form-control mb-3" />
+
+                            {estadoEditar ?
                             
+                             <button type="submit" className="btn btn-info" onClick={(e) =>cambio(e)}>Editar</button>
+
+                            : 
+                            <button type="submit" className="btn btn-primary">Agregar</button>
+}
                         </form>
+                        {/*  FINALIZA INGRESO DE TAREA */}
                     </div>
                 </div>
             </div>
